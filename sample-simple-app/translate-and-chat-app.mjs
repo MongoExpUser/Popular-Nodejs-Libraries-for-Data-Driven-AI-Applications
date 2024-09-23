8aad717f13f8589cd4e3417c565e70dda22c4aa9
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2024-09-23 18:45:02 -0500

    Commit

--- sample-simple-app/translate-and-chat-app.mjs
+++ sample-simple-app/translate-and-chat-app.mjs
@@ -1,13 +1,13 @@
-/******************************************************************************************************************************
-*                                                                                                              				        *
-* Project:  Translations and Chats App with Nodejs Fetch API                                            							        *
-*                                                                                                              				        *
-*  Copyright © 2024. MongoExpUser.  All Rights Reserved.                                                       				        *
-*                                                                                                              				        *
-*  License: MIT - https://github.com/MongoExpUser/Popular-Nodejs-Libraries-for-Data-Driven-AI-Applications/blob/main/LICENSE  *
-*                                                                                                                     				*
-*                                                                                                                     				*
-/*****************************************************************************************************************************/
+/********************************************************************************************************************************
+*                                                                                                              			*
+* Project:  Translations and Chats App with Nodejs Fetch API                                            			*
+*                                                                                                              			*
+*  Copyright © 2024. MongoExpUser.  All Rights Reserved.                                                       			*
+*                                                                                                              			*
+*  License: MIT - https://github.com/MongoExpUser/Popular-Nodejs-Libraries-for-Data-Driven-AI-Applications/blob/main/LICENSE    *
+*                                                                                                                     		*
+*                                                                                                                     		*
+/*******************************************************************************************************************************/
 
 // translate-and-chat-app.js
 
